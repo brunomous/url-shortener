31076ba593befe5af16294fb05f521bcf9967367
Jefferson Silva <brnmouras@gmail.com>
2026-06-09 23:10:31 -0300

    refactor: improve database schema

--- src/db/scripts.ts
+++ src/db/scripts.ts
@@ -5,16 +5,18 @@ DROP TABLE IF EXISTS urls;
 export const createTable = `
 CREATE TABLE urls (
   id SERIAL PRIMARY KEY,
-  original_url VARCHAR(255) NOT NULL,
+  original_url TEXT NOT NULL,
   hash VARCHAR(255) NOT NULL,
-  short_url VARCHAR(255) NOT NULL,
-  slug VARCHAR(255) NOT NULL
+  slug VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  clicks INTEGER NOT NULL DEFAULT 0
 );
 `;
 
 export const seedEntry = `
-INSERT INTO urls (original_url, hash, short_url, slug) VALUES
-  ('https://www.google.com', '12345678', 'https://short.url/google', '/')
+INSERT INTO urls (original_url, hash, slug) VALUES
+  ('https://www.google.com', '12345678', '/');
 `;
 
 export const listEntries = `
